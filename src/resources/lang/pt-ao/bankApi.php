9edf7bd8aef11a1f2ffdc5cc631bdf99de41a3fd
vladimir.pereira <vladimir.pereira@codificar.com.br>
2020-09-23 16:35:39 -0300

    Add pt-ao translations

---
+++ src/resources/lang/pt-ao/bankApi.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'name_required'           => 'O nome do banco é requerido',
+    'code_required'           => 'O código do banco é requerido',
+    'code_unique'             => 'O código informado já está cadastrado'
+);
