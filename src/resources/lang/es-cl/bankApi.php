45ea66ba360ceb2cc851684551cbd2a40a7591fb
vladimir.pereira <vladimir.pereira@codificar.com.br>
2020-12-16 16:10:08 -0300

    Add ESCL translations

---
+++ src/resources/lang/es-cl/bankApi.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'name_required'           => 'El nombre del banco es obligatorio.',
+    'code_required'           => 'El código del banco es obligatorio',
+    'code_unique'             => 'El código del banco ya existe'
+);
