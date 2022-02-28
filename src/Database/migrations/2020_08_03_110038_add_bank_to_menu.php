c2b4a462f8f50a4cb385e83063c3455813643728
Hari Dasa <hari.dasa@codificar.com.br>
2022-02-28 15:30:22 -0300

    change seeder namespace

--- src/Database/migrations/2020_08_03_110038_add_bank_to_menu.php
+++ src/Database/migrations/2020_08_03_110038_add_bank_to_menu.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-use Codificar\Bank\Database\seeds\BankMenuSeeder;
+use Codificar\Bank\Database\seeders\BankMenuSeeder;
 
 class AddBankToMenu extends Migration
 {
